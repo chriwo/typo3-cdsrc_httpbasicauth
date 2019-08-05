b11cf897903e23c8373a258aeacf2db9b140e75d
Christian Wolfram <c.wolfram@chriwo.de>
2019-08-06 00:02:33 +0200

    [TASK] Allow usage with TYPO3 v9

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,7 +34,7 @@ $EM_CONF[$_EXTKEY] = [
     'CGLcompliance_note' => null,
     'constraints' => [
         'depends' => [
-            'typo3' => '7.0.0-8.7.99',
+            'typo3' => '7.0.0-9.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
